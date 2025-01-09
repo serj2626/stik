7b754e1a229b77a33db40019537bb10a0a5177db
serj2626 <serj2626@mail.ru>
2025-01-09 21:06:45 +0300

    feat: add import product list

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,46 @@
 <script>
 export default {
-  name: 'IndexPage'
+
 }
 </script>
 <template>
 
+  <ProductList />
+    <!-- <div class="container galery">
+      <p class="galery__title">Похожие товары</p>
+      <div class="galery__cards">
+        <div class="card">
+          <img class="card__img" src="assets/images/similar_products/Image_1.jpg" alt="">
+          <div class="card__body">
+            <p class="card__title">ПЛАТЬЕ С V-ОБРАЗНЫМ ВЫРЕЗОМ, БЕЛЫЙ</p>
+            <div class="card__price">14 900 RUB</div>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card__img" src="assets/images/similar_products/Image_2.jpg" alt="">
+          <div class="card__body">
+            <p class="card__title">ЖАКЕТ ДВУБОРТНЫЙ, СЕРО-ГОЛУБОЙ</p>
+            <div class="card__price">8900 RUB</div>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card__img" src="assets/images/similar_products/Image_3.jpg" alt="">
+          <div class="card__body">
+            <p class="card__title">ПЛАТЬЕ МАКСИ С ЯРУСАМИ, БЕЛЫЙ</p>
+            <div class="card__price">10 500 RUB</div>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card__img" src="assets/images/similar_products/Image_4.jpg" alt="">
+          <div class="card__body">
+            <p class="card__title">КОМБИНЕЗОН СО СТОЙКОЙ, ЧёРНЫЙ</p>
+            <div class="card__price">9500 RUB</div>
+          </div>
+        </div>
+      </div>
+    </div> -->
 </template>
+
+<style>
+
+</style>
