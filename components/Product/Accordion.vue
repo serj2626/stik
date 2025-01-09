77d006536e75699955a71740b9eb8f1289d1c570
serj2626 <serj2626@mail.ru>
2025-01-09 21:40:01 +0300

    feat: new detail component

---
+++ components/Product/Accordion.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  props: {
+    accordion: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <div class="accordion__item">
+    <div class="accordion__title">{{ accordion.title }}</div>
+    <div class="accordion__action">
+      <img :src="accordion.btnSrc" alt="" />
+    </div>
+  </div>
+</template>
+
+<style>
+.accordion__item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 360px;
+  padding-block: 15px;
+
+  border-block: 1px solid #e0e0e0;
+}
+
+.accordion__title {
+  text-transform: uppercase;
+  font-size: 10px;
+  line-height: 14px;
+  color: #333333;
+}
+
+.accordion__action {
+  padding-right: 7px;
+}
+</style>
