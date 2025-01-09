c1aabd760a330ba275d8c3e8ba1725de1128421f
serj2626 <serj2626@mail.ru>
2025-01-09 21:05:51 +0300

    feat: created data for galery

---
+++ assets/data/products.data.js
@@ -0,0 +1,22 @@
+export const productsData = [
+  {
+    title: "ПЛАТЬЕ С V-ОБРАЗНЫМ ВЫРЕЗОМ, БЕЛЫЙ",
+    src: require("~/assets/images/similar_products/Image_1.jpg"),
+    price: "14 900 RUB"
+  },
+  {
+    title: "ЖАКЕТ ДВУБОРТНЫЙ, СЕРО-ГОЛУБОЙ",
+    src: require("~/assets/images/similar_products/Image_2.jpg"),
+    price: "8900 RUB"
+  },
+  {
+    title: "ПЛАТЬЕ МАКСИ С ЯРУСАМИ, БЕЛЫЙ",
+    src: require("~/assets/images/similar_products/Image_3.jpg"),
+    price: "10 500 RUB"
+  },
+  {
+    title: "КОМБИНЕЗОН СО СТОЙКОЙ, ЧёРНЫЙ",
+    src: require("~/assets/images/similar_products/Image_4.jpg"),
+    price: "9500 RUB"
+  },
+];
