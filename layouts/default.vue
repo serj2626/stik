aad13fdc9446ebc785849d84bf534126de0c532e
serj2626 <serj2626@mail.ru>
2025-01-09 20:48:59 +0300

    feat: add import component Header

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+  },
+};
+</script>
+<template>
+  <div>
+   <Header />
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<style>
+
+</style>
