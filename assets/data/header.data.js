dcbb723cdb321805425dcfd3999d5b632bdad3a8
serj2626 <serj2626@mail.ru>
2025-01-09 20:47:37 +0300

    feat: created header links data

---
+++ assets/data/header.data.js
@@ -0,0 +1,18 @@
+export const headerDataLinks = [
+  {
+    title: "Search",
+    src: require("~/assets/images/Search.png"),
+  },
+  {
+    title: "Stroke",
+    src: require("~/assets/images/Stroke.png"),
+  },
+  {
+    title: "User",
+    src: require("~/assets/images/User.png"),
+  },
+  {
+    title: "Basket",
+    src: require("~/assets/images/Basket.png"),
+  },
+];
