93cec62f71533104ed12a7331703f5d99cf4a10f
serj2626 <serj2626@mail.ru>
2025-01-09 21:14:01 +0300

    feat: added props

--- components/Product/Item.vue
+++ components/Product/Item.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
-  name: "HelloWorld",
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
   data() {
     return {
 
@@ -15,9 +20,35 @@ export default {
 };
 </script>
 <template>
-
+      <div class="card">
+        <img class="card__img" :src="product.src" :alt="product.title" />
+        <div class="card__body">
+          <p class="card__title">{{ product.title }}</p>
+          <div class="card__price">{{ product.price }}</div>
+        </div>
+      </div>
 </template>
 
 <style scoped>
 
+.card {
+  margin-bottom: 100px;
+  cursor: pointer;
+  transition: box-shadow 0.2s ease-in-out;
+
+  &:hover {
+    box-shadow: 10px 10px 5px 0 #0000005a;
+  }
+}
+.card__body {
+  font-size: 10px;
+  line-height: 14px;
+  padding-left: 15px;
+  padding-bottom: 7px;
+}
+
+.card__title {
+  margin-top: 15px;
+  margin-bottom: 10px;
+}
 </style>
