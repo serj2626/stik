2aac4cf54c8a269c63ffdb3d0dc37bf2c43a34f7
serj2626 <serj2626@mail.ru>
2025-01-09 21:39:40 +0300

    feat: new data for main card

---
+++ assets/data/card.data.js
@@ -0,0 +1,30 @@
+export const mainCard = {
+  galerySrc: require("~/assets/images/products/Galery.jpg"),
+  src: require("~/assets//images/products/main.jpg"),
+  title: "ЖАКЕТ УДЛИНЁННЫЙ, БЕЛЫЙ",
+  price: "890 ₽",
+  sizes: [
+    {
+      title: "XS",
+      count: "мало",
+    },
+    {
+      title: "S",
+      count: undefined,
+    },
+    {
+      title: "M",
+      count: "подписка",
+    },
+  ],
+  accordion: [
+    {
+      title: "Описание",
+      btnSrc: require("~/assets/images/plus.svg"),
+    },
+    {
+      title: "Состав и Уход",
+      btnSrc: require("~/assets/images/plus.svg"),
+    },
+  ],
+};
